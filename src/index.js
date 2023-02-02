3c8856b3503f8becb4d9877a446749f5cc04eadf
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2023-02-02 09:55:11 +0200

    instal-librarys
    lodash/spinner/notidlix/axios/firebase

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import { initializeApp } from 'firebase/app';
+
+// import Notiflix from 'notiflix';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// import axios from 'axios';
+
+// import {Spinner} from 'spin.js';
+
+// lodash
