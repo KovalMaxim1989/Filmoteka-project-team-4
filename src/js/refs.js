2327d99e24ce9d17980805e05b43c4edcf7edca9
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2023-02-02 20:34:47 +0200

    data service and refs

---
+++ src/js/refs.js
@@ -0,0 +1,24 @@
+export const refs = {
+  moviesList: document.querySelector('.js-films-list'),
+
+  // ЯКЩО КОМУ ПОТРІБНІ РЕФИ АБО ШУКАЙТЕ ВЖЕ СТВОРЕНІ І РОЗКОМЕНТОВУЙТЕ АБО ДОДАВАЙТЕ СВОЇ
+
+  // Modal elements
+  // modalContainer: document.querySelector('.js-modal-container'),
+  // closeModalBtn: document.querySelector('[data-modal-close]'),
+  // modal: document.querySelector('[data-modal]'), // backdrop
+  // btnAddToQueue: document.querySelector('[data-add-to-queue]'),
+  // btnAddToWatched: document.querySelector('[data-add-to-watched]'),
+
+  // // Search input in Header
+  // searchForm: document.querySelector('.header__form'),
+
+  // // spiner
+  // backdropSpinner: document.querySelector('[data-load]'),
+  // spinner: document.querySelector('.js-spinner'),
+
+  // // Library page
+  // libraryList: document.querySelector('.js-library-list'),
+  // btnQueue: document.querySelector('.js-btn-queue'),
+  // btnWatched: document.querySelector('.js-btn-watched'),
+};
