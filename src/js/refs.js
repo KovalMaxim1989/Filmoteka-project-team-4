2216d6c51434b5b159fa95263795b5ce70807769
PavloZiuskin <passzuskin@gmail.com>
2023-02-03 16:52:22 +0200

    add-pagination-index-html

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,20 @@
+import {
+  onClickIncrementPage,
+  onClickDecrementPage,
+  onClickPaginationBtnNumber,
+} from './pagination';
 export const refs = {
   moviesList: document.querySelector('.js-films-list'),
+  firstPage: document.querySelector('.js-pagination--firstPage'),
+  minus2Page: document.querySelector('.js-pagination--minus2Page'),
+  minus1Page: document.querySelector('.js-pagination--minus1Page'),
+  activPage: document.querySelector('.js-pagination--activPage'),
+  plus1Page: document.querySelector('.js-pagination--plus1Page'),
+  plus2Page: document.querySelector('.js-pagination--plus2Page'),
+  lastPage: document.querySelector('.js-pagination--lastPage'),
+  minusQuery: document.querySelector('.js-btn__minusQuery'),
+  plusQuery: document.querySelector('.js-btn__plusQuery'),
+  containerPage: document.querySelector('.js-pagination__page'),
 
   // ЯКЩО КОМУ ПОТРІБНІ РЕФИ АБО ШУКАЙТЕ ВЖЕ СТВОРЕНІ І РОЗКОМЕНТОВУЙТЕ АБО ДОДАВАЙТЕ СВОЇ
 
@@ -22,3 +37,7 @@ export const refs = {
   // btnQueue: document.querySelector('.js-btn-queue'),
   // btnWatched: document.querySelector('.js-btn-watched'),
 };
+
+refs.minusQuery.addEventListener('click', onClickDecrementPage);
+refs.plusQuery.addEventListener('click', onClickIncrementPage);
+refs.containerPage.addEventListener('click', onClickPaginationBtnNumber);
