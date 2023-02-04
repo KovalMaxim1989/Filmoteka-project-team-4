b5abfad67f2ead5f42a9831992e8b356b22b1e36
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-02-04 20:36:22 +0200

    markup-library-card
    edit markup library card

--- src/js/markup.js
+++ src/js/markup.js
@@ -138,29 +138,26 @@ export function createMarkupLibraryList(moviesData) {
 
       return `<li class="films__item js-target" data-id="${id}">
                   <div class="films__img-wrapper">
-            <img
-               src="${
-                 poster_path
-                   ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                   : defaultImg
-               }"
-              alt="${title}"
-              class="films__img" loading="lazy"
-            />
-          </div>
-          <div class="films__info">
-            
-            <p class="films__name">${title}</p>
-            <p class="films__desk">
-              <span class="films__genre">${filterGenres}</span> |
-              <span class="films__year">${Number.parseInt(release_date)}</span>
-            </p>
-            <p class="films__desk">
-              <span class="films__rating--text"> Rating: </span>
-              <span class="films__rating">${vote_average.toFixed(1)}</span>
-            </p>
-          </div>
-      </li>`;
+                   <img
+                    src="${poster_path? `https://image.tmdb.org/t/p/w500${poster_path}` : defaultImg}"
+                    alt="${title}"
+                    class="films__img" loading="lazy"
+                   />
+                   <div class="rating">
+                     <p class="films__desk">
+                       <span class="films__rating--text"> Rating: </span>
+                       <span class="films__rating">${vote_average.toFixed(1)}</span>
+                     </p>
+                    </div>
+                  </div>
+               <div class="films__info">
+                 <p class="films__name">${title}</p>
+                 <p class="films__desk">
+                   <span class="films__genre">${filterGenres}</span> |
+                    <span class="films__year">${Number.parseInt(release_date)}</span>
+                  </p>
+               </div>
+             </li>`;
     })
     .join('');
 }
