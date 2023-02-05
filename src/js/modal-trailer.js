41b1597830f57b16e1519dd6ac25a5ea6a4eeebe
SergeiTkachenko <fox.sergei92@gmail.com>
2023-02-05 17:07:18 +0200

    fix treiler btn

--- src/js/modal-trailer.js
+++ src/js/modal-trailer.js
@@ -9,6 +9,7 @@ const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const API_KEY = 'd03712107dcdd723f1173c5ee2c0d8c1';
 
 async function fetchTrailerKey(currentId) {
+  refs.trailerBtn.classList.remove('visually-hidden');
   //   spinnerOn();
   try {
     const response = await axios.get(
@@ -18,7 +19,7 @@ async function fetchTrailerKey(currentId) {
 
     return key;
   } catch (error) {
-    Notiflix.Notify.failure('Sorry, there is no trailer for this movie');
+    refs.trailerBtn.classList.add('visually-hidden');
   }
   //   spinnerOff();
 }
