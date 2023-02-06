13339a6a8350ed468649d95276d7fdda21046508
SergeiTkachenko <fox.sergei92@gmail.com>
2023-02-07 00:37:37 +0200

    add filter

--- src/js/search-films.js
+++ src/js/search-films.js
@@ -7,6 +7,7 @@ import { pagination } from './pagination';
 import { refs } from './refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Spinner } from './spinner';
+
 const spinner = new Spinner();
 const dataService = new DataService();
 
@@ -57,6 +58,103 @@ function onSearch(e) {
         return;
       }
       refs.containerPagAll.classList.remove('visually-hidden');
+      pagination(data);
+
+      const necessaryData = dataService.getDataTrendMovies(data.results);
+      const markupTrendMovies = createMarkupFilmsList(necessaryData);
+      refs.moviesList.innerHTML = markupTrendMovies;
+    })
+    .catch(err => Notify.failure(err))
+    .finally(() => {
+      spinner.stop();
+    });
+}
+
+//////////filter////////////
+refs.filterNowPlaying.addEventListener('click', getNowPlaying);
+
+function getNowPlaying(e) {
+  e.preventDefault();
+
+  refs.moviesList.innerHTML = '';
+
+  spinner.start();
+
+  movieAPI
+    .getNowPlaying()
+    .then(data => {
+      pagination(data);
+
+      const necessaryData = dataService.getDataTrendMovies(data.results);
+      const markupTrendMovies = createMarkupFilmsList(necessaryData);
+      refs.moviesList.innerHTML = markupTrendMovies;
+    })
+    .catch(err => Notify.failure(err))
+    .finally(() => {
+      spinner.stop();
+    });
+}
+
+refs.filterPopular.addEventListener('click', getPopular);
+
+function getPopular(e) {
+  e.preventDefault();
+
+  refs.moviesList.innerHTML = '';
+
+  spinner.start();
+
+  movieAPI
+    .getPopular()
+    .then(data => {
+      pagination(data);
+
+      const necessaryData = dataService.getDataTrendMovies(data.results);
+      const markupTrendMovies = createMarkupFilmsList(necessaryData);
+      refs.moviesList.innerHTML = markupTrendMovies;
+    })
+    .catch(err => Notify.failure(err))
+    .finally(() => {
+      spinner.stop();
+    });
+}
+
+refs.filterTopRated.addEventListener('click', getTopRated);
+
+function getTopRated(e) {
+  e.preventDefault();
+
+  refs.moviesList.innerHTML = '';
+
+  spinner.start();
+
+  movieAPI
+    .getTopRated()
+    .then(data => {
+      pagination(data);
+
+      const necessaryData = dataService.getDataTrendMovies(data.results);
+      const markupTrendMovies = createMarkupFilmsList(necessaryData);
+      refs.moviesList.innerHTML = markupTrendMovies;
+    })
+    .catch(err => Notify.failure(err))
+    .finally(() => {
+      spinner.stop();
+    });
+}
+
+refs.filterUpcoming.addEventListener('click', getUpcoming);
+
+function getUpcoming(e) {
+  e.preventDefault();
+
+  refs.moviesList.innerHTML = '';
+
+  spinner.start();
+
+  movieAPI
+    .getUpcoming()
+    .then(data => {
       pagination(data);
 
       const necessaryData = dataService.getDataTrendMovies(data.results);
