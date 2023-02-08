0186a9675fa987e051f7a00b3c62bf89275abb1b
KobzarAnatolii <TohaPirat@gmail.com>
2023-02-08 10:39:46 +0200

    спинер на модалке

--- src/js/modal-trailer.js
+++ src/js/modal-trailer.js
@@ -3,7 +3,10 @@ import Notiflix from 'notiflix';
 
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
+import { Spinner } from './spinner';
+
 // import { refs } from './refs';
+const spinner = new Spinner();
 
 const refs = {
   backdrop: document.querySelector('.backdrop'),
@@ -18,18 +21,17 @@ const API_KEY = 'd03712107dcdd723f1173c5ee2c0d8c1';
 
 async function fetchTrailerKey(currentId) {
   refs.trailerBtn.classList.remove('visually-hidden');
-  //   spinnerOn();
+  spinner.start();
   try {
     const response = await axios.get(
       `${BASE_URL}${currentId}/videos?api_key=${API_KEY}`
     );
     const key = response.data.results[0].key;
-
+    spinner.stop();
     return key;
   } catch (error) {
     refs.trailerBtn.classList.add('visually-hidden');
   }
-  //   spinnerOff();
 }
 
 export { fetchTrailerKey };
